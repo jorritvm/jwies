f9647f7e8ad3c62eb3b38a769cdd88fdfc311fc9
jorritvm <jorrit.vm@gmail.com>
2020-05-11 22:46:52 +0200

    controller settings pane and starting of player ui

--- main_controller.py
+++ main_controller.py
@@ -3,11 +3,12 @@ from PyQt5.QtGui import *
 from PyQt5.QtWidgets import *
 from PyQt5.QtNetwork import *
 from PyQt5 import uic
+import configparser
 
 import sys
 import os
 import requests
-
+import time
 
 class Controller(QMainWindow):
 
@@ -15,11 +16,12 @@ class Controller(QMainWindow):
         super(Controller, self).__init__(*args, **kwargs)
 
         self.setup_gui()
+        self.settings = self.load_settings()
         self.log("Wies controller initiated")
 
     def setup_gui(self):
         # gui elements
-        self.setWindowTitle("Wies controller")
+        self.setWindowTitle("jwies - controller screen")
         self.setWindowIcon(QIcon(os.path.join("icons", "server.png")))
 
         self.input_ip = QLineEdit()
@@ -62,8 +64,14 @@ class Controller(QMainWindow):
         ip_dialog_action.triggered.connect(lambda x: self.show_ip_dialog())
         settings_pane_action.triggered.connect(lambda x: self.show_settings_pane())
 
+    def load_settings(self):
+        settings = configparser.ConfigParser()
+        settings.read(os.path.join("settings", "controller.ini"))
+        return(settings)
+
     def log(self, txt):
-        self.textbox.appendPlainText(txt)
+        msg = "(%s) %s" % (time.strftime('%H:%M:%S'), txt)
+        self.textbox.appendPlainText(msg)
 
     def get_ip(self):
         return(requests.get('https://api.ipify.org').text)
@@ -87,9 +95,46 @@ class Controller(QMainWindow):
         text, okPressed = QInputDialog.getText(self, "External ip lookup", "Your external ip is:", QLineEdit.Normal, self.get_ip())
 
     def show_settings_pane(self):
-        settings_pane = uic.loadUi("settings_pane.ui")
-        settings_pane.exec_()
+        settings_pane = uic.loadUi(os.path.join("ui", "settings_pane.ui"))
+        settings_pane.setWindowIcon(QIcon(os.path.join("icons", "network-hub.png")))
 
+        # set initial value for dialog box
+        settings_pane.check_dealer_shuffle.setChecked(self.settings.getboolean('game', 'dealer_can_shuffle'))
+        settings_pane.check_misere_beats_trull.setChecked(self.settings.getboolean('game', 'misere_ouverte_beats_trull'))
+        settings_pane.spin_split_minimum.setValue(self.settings.getint('game', 'minimum_cards_to_cut'))
+        settings_pane.spin_split_maximum.setValue(self.settings.getint('game', 'maximum_cards_to_cut'))
+
+        settings_pane.spin_points_exact.setValue(self.settings.getint('points', 'exact_amount_of_tricks'))
+        settings_pane.spin_points_surpass.setValue(self.settings.getfloat('points', 'extra_trick'))
+        settings_pane.spin_points_abondance.setValue(self.settings.getint('points', 'abundance'))
+        settings_pane.spin_points_misere.setValue(self.settings.getint('points', 'misere'))
+        settings_pane.spin_points_misere_ouverte.setValue(self.settings.getint('points', 'misere_ouverte'))
+        settings_pane.spin_points_solo.setValue(self.settings.getint('points', 'solo'))
+        settings_pane.spin_points_soloslim.setValue(self.settings.getint('points', 'solo_slim'))
+        settings_pane.check_lose_double.setChecked(self.settings.getboolean('points', 'losing_is_double'))
+        settings_pane.check_pass_double.setChecked(self.settings.getboolean('points', 'when_all_pass_points_are_double'))
+
+        if settings_pane.exec_():
+            # save new values in settings
+            self.settings["game"]["dealer_can_shuffle"] = "True" if settings_pane.check_dealer_shuffle.isChecked() else "False"
+            self.settings["game"]["misere_ouverte_beats_trull"] = "True" if settings_pane.check_misere_beats_trull.isChecked() else "False"
+            self.settings["game"]["minimum_cards_to_cut"] = str(settings_pane.spin_split_minimum.value())
+            self.settings["game"]["maximum_cards_to_cut"] = str(settings_pane.spin_split_maximum.value())
+
+            self.settings["points"]["exact_amount_of_tricks"] = str(settings_pane.spin_points_exact.value())
+            self.settings["points"]["extra_trick"] = str(settings_pane.spin_points_surpass.value())
+            self.settings["points"]["abundance"] = str(settings_pane.spin_points_abondance.value())
+            self.settings["points"]["misere"] = str(settings_pane.spin_points_misere.value())
+            self.settings["points"]["misere_ouverte"] = str(settings_pane.spin_points_misere_ouverte.value())
+            self.settings["points"]["solo"] = str(settings_pane.spin_points_solo.value())
+            self.settings["points"]["solo_slim"] = str(settings_pane.spin_points_soloslim.value())
+            self.settings["points"]["losing_is_double"] = "True" if settings_pane.check_lose_double.isChecked() else "False"
+            self.settings["points"]["when_all_pass_points_are_double"] = "True" if settings_pane.check_pass_double.isChecked() else "False"
+
+            with open(os.path.join("settings", "controller.ini"), 'w') as settings_file:
+                self.settings.write(settings_file)
+
+            self.log("Controller settings saved")
 
 
 class TcpServer(QTcpServer):
--- main_player.py
+++ main_player.py
@@ -1,1 +1,112 @@
-print('hello world')
+from PyQt5.QtCore import *
+from PyQt5.QtGui import *
+from PyQt5.QtWidgets import *
+from PyQt5.QtNetwork import *
+from PyQt5 import uic
+import configparser
+
+import sys
+import os
+import time
+
+class Player(QMainWindow):
+
+    def __init__(self, *args, **kwargs):
+        super(Player, self).__init__(*args, **kwargs)
+
+        self.setup_gui()
+        self.settings = self.load_settings()
+        self.log("Wies player initiated")  # self.connect_to_a_game()
+
+    def setup_gui(self):
+        # gui elements
+        self.setWindowTitle("jwies - player screen")
+        self.setWindowIcon(QIcon(os.path.join("icons", "playing-card.png")))
+
+        # table
+        self.table = QLabel("playing field placeholder")
+
+        # bidoptions
+        self.bidoptions = QLabel("bidoptions placeholder")
+
+        # chat window
+        self.textbox = QPlainTextEdit()
+        self.textbox.setReadOnly(True)
+
+        # chat input window
+        self.inputbox = QPlainTextEdit()
+
+        # layouts
+        layoutleft = QVBoxLayout()
+        layoutleft.addWidget(self.table)
+        layoutleft.addWidget(self.bidoptions)
+        leftwidget = QWidget()
+        leftwidget.setLayout(layoutleft)
+
+        # layoutright = QVBoxLayout()
+        rightsplitter = QSplitter()
+        rightsplitter.setOrientation(Qt.Vertical)
+        rightsplitter.addWidget(self.textbox)
+        rightsplitter.addWidget(self.inputbox)
+
+        centralsplitter = QSplitter()
+        centralsplitter.addWidget(leftwidget)
+        centralsplitter.addWidget(rightsplitter)
+
+        centrallayout = QHBoxLayout()
+        centrallayout.addWidget(centralsplitter)
+        central_widget = QWidget()
+        central_widget.setLayout(centrallayout)
+        self.setCentralWidget(central_widget)
+
+        # actions
+        connect_action = QAction("Connect", self)
+        about_action = QAction("About", self)
+
+        menubar = self.menuBar()
+        menu = menubar.addMenu("&Menu")
+        menu.addAction(connect_action)
+        menu.addAction(about_action)
+
+        # size
+        self.resize(800, 600)
+
+        # signals & slots
+        connect_action.triggered.connect(lambda x: self.connect_to_a_game())
+        # self.start_server.clicked.connect(lambda x: self.start_tcp_server(self.input_ip.text(), self.input_port.text()))
+        # self.close_server.clicked.connect(lambda x: self.stop_tcp_server())
+        # settings_pane_action.triggered.connect(lambda x: self.show_settings_pane())
+
+    def log(self, txt):
+        msg = "(%s) %s" % (time.strftime('%H:%M:%S'), txt)
+        self.textbox.appendPlainText(msg)
+
+    def load_settings(self):
+        settings = configparser.ConfigParser()
+        settings.read(os.path.join("settings", "player.ini"))
+        return (settings)
+
+    def connect_to_a_game(self):
+        connect_pane = uic.loadUi(os.path.join("ui", "connect_pane.ui"))
+        connect_pane.setWindowIcon(QIcon(os.path.join("icons", "network-hub.png")))
+
+        # set initial value for dialog box
+        connect_pane.line_name.setText(self.settings['last_connection']['name'])
+        connect_pane.line_host.setText(self.settings['last_connection']['host'])
+        connect_pane.spin_port.setValue(self.settings.getint('last_connection', 'port'))
+
+        if connect_pane.exec_():
+            # save new values in settings
+            self.settings["last_connection"]["name"] = connect_pane.line_name.text()
+            self.settings["last_connection"]["host"] = connect_pane.line_host.text()
+            self.settings["last_connection"]["port"] = str(connect_pane.spin_port.value())
+
+            with open(os.path.join("settings", "player.ini"), "w") as settings_file:
+                self.settings.write(settings_file)
+
+            self.log("Player settings saved")
+
+app = QApplication(sys.argv)
+main = Player()
+main.show()
+app.exec_()
